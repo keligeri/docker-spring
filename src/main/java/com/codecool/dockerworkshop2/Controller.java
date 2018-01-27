af4482fd9a54496633ae8b927aa9719d89ec2752
Aron Kovacs <kovacs.aron@kovaron.com>
2018-01-27 12:56:07 +0100

    db

--- src/main/java/com/codecool/dockerworkshop2/Controller.java
+++ src/main/java/com/codecool/dockerworkshop2/Controller.java
@@ -1,10 +1,16 @@
 package com.codecool.dockerworkshop2;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 public class Controller {
+
+    @Autowired
+    private UserRepository userRepository;
 
     @CrossOrigin
     @RequestMapping(value = "/sayhello", method = RequestMethod.GET)
@@ -14,6 +20,12 @@ public class Controller {
             msg += " " + name;
         }
         return Response.buildSuccess(msg);
+    }
+
+    @CrossOrigin
+    @RequestMapping(value = "/users", method = RequestMethod.GET)
+    public List<User> list(){
+        return userRepository.findAll();
     }
 
 }
